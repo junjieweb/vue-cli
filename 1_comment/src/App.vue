6061cfeab25d98e7d4de997194c2201a75656028
junjieweb <297901657@qq.com>
2022-02-19 13:56:35 +0800

    add code

---
+++ 1_comment/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <Header></Header>
+    <div class="container">
+      <Add></Add>
+      <List></List>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Add from "@/components/Add";
+import List from '@/components/List';
+export default {
+  name: "App",
+  components: {
+    Header,
+    Add,
+    List
+  },
+};
+</script>
+
+<style>
+.reply {
+  margin-top: 0;
+}
+
+li {
+  transition: 0.5s;
+  overflow: hidden;
+}
+
+.handle {
+  width: 40px;
+  border: 1px solid #ccc;
+  background: #fff;
+  position: absolute;
+  right: 10px;
+  top: 1px;
+  text-align: center;
+}
+
+.handle a {
+  display: block;
+  text-decoration: none;
+}
+
+.list-group-item .centence {
+  padding: 0px 50px;
+}
+
+.user {
+  font-size: 22px;
+}
+</style>
---
+++ 1_comment/src/components/Item.vue
@@ -0,0 +1,26 @@
+<template>
+  <li class="list-group-item">
+    <div class="handle">
+      <a href="javascript:;">删除</a>
+    </div>
+    <p class="user"><span>yyy</span><span>说:</span></p>
+    <p class="centence">Vue有点难!</p>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "Item",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
